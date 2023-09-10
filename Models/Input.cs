d78453bbb75ee0c0831adf5b11414e8d9d46bd51
PJdeH <60782437+PJdeH@users.noreply.github.com>
2023-09-10 15:17:20 +0200

    added files
    added files

---
+++ Models/Input.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NumberSelector.Models
+{
+    public class Input
+    {
+        [Required(ErrorMessage = "[x,y,..,.....] is required and only integers")]
+        public string Values { get; set; }
+    }
+
+    public class TotalNumbers
+    {
+        public int Value { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using NumberSelector.Models;
+using System.Text.RegularExpressions;
+
+namespace NumberSelector.Pages;
+
+public class IndexModel : PageModel
+{
+    public Input Values { get; set; }
+    public void OnGet()
+    {
+    }
+
+    public void OnPostSubmit(Input values)
+    {
+        if (ModelState.IsValid)
+        {
+            string valueList = values.Values;
+            string respons = GetUniqueValues(valueList);
+            ViewData["Values"] = respons;
+        }
+    }
+
+    private static string GetUniqueValues(string valueList)
+    {
+        string response;
+        Regex regex = new Regex(@"^\[(\d*\,)*\d*\]$");
+
+        if (regex.IsMatch(valueList))
+        {
+
+            List<int> intList = valueList.Substring(1, valueList.Length - 2).Split(",").ToList().ConvertAll(int.Parse);
+            List<TotalNumbers> uniqueList = new List<TotalNumbers>();
+
+            foreach (var number in intList)
+            {
+                if (uniqueList.Exists(x => x.Value == number))
+                {
+                    uniqueList.First(x => x.Value == number).Count++;
+                }
+                else
+                {
+                    var newNumber = new TotalNumbers()
+                    {
+                        Value = number,
+                        Count = 1
+                    };
+
+                    uniqueList.Add(newNumber);
+                }
+            }
+
+            var uniqueNumbers = uniqueList.Where(x => x.Count > 2).OrderByDescending(x => x.Value);
+            response = string.Join(",", uniqueNumbers.Select(n => n.Value.ToString()).ToArray());
+
+            response = "[" + response + "]";
+        }
+        else
+        {
+            response = "Please give input into the following format: [x,y,z,..,......] where x, y,z , .. are all integers, example:[1,23,3,3,89]";
+        }
+
+        return response;
+    }
+}
